6e29a3a0f1964dd3feaf2c4da62c7477a2717313
jinjulee <genesis0210@gmail.com>
2022-06-25 02:05:47 +0900

    Changes

---
+++ src/test/kotlin/com/juri01/webfluxstudy/WebfluxStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.juri01.webfluxstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
